aeec58a2213a2b03fa66323059c2a6e77fd6a912
fanpengpeng <13314209626@163.com>
2019-10-26 20:54:58 +0800

    添加路由、添加全局样式、引入element-ui插件

---
+++ src/views/Login/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <el-button>按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: ""
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  height: 100vh;
+  background: pink;
+}
+</style>
